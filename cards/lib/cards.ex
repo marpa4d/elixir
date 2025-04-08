a0adde134896a376fcb82d91cf948b75fce07578
Marpa4D <Marpa3d@yandex.ru>
2025-04-08 14:15:22 +0300

    Create func hello

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,5 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def hello do
-    :world
+    "Привет из модуля Cards!"
   end
 end
