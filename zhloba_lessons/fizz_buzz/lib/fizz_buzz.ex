c6930421d98c4245d415d18437073e9c305eaa8f
Marpa4D <Marpa3d@yandex.ru>
2025-04-09 13:55:33 +0300

    Рефакторинг функции fizz_buzz(n)

--- zhloba_lessons/fizz_buzz/lib/fizz_buzz.ex
+++ zhloba_lessons/fizz_buzz/lib/fizz_buzz.ex
@@ -4,12 +4,15 @@ defmodule FizzBuzz do
   Enum.each(1..100, &fizz_buzz/1)
  end
 
+ @spec fizz_buzz(integer()) :: String.t()
  def fizz_buzz(n) do
+   div_to_3 = rem(n, 3) == 0
+   div_to_5 = rem(n, 5) == 0
    cond do
-     rem(n, 3) == 0 and rem(n, 5) == 0 -> IO.puts("FizzBuzz")
-     rem(n, 3) == 0 -> IO.puts("Fizz")
-     rem(n, 5) == 0 -> IO.puts("Buzz")
-     true -> IO.puts(n)
+     div_to_3 and div_to_5 -> "FizzBuzz"
+     div_to_3 -> "Fizz"
+     div_to_5 -> "Buzz"
+     true -> to_string(n)
    end
  end
 end
