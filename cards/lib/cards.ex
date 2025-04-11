354facadf2d968f69d086140b30e73275113de63
Marpa4D <Marpa3d@yandex.ru>
2025-04-11 17:31:12 +0300

    Создана функция contains? и расширен список до пяти карт

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,9 +1,15 @@
 defmodule Cards do
+
   def create_deck do
-    ["Туз", "Двойка", "Тройка"]
+    ["Туз", "Двойка", "Тройка", "Четверка", "Пятерка"]
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
 end
